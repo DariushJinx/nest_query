90d689e22c73c1bedd1ecc6c46016e315950a511
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-09 14:40:21 +0330

    handle config file for project

---
+++ src/config.ts
@@ -0,0 +1,4 @@
+export const ACCESS_TOKEN_SECRET_KEY =
+  'CCEBD888251E547C877C43A324B5DE12468F431EA0D19A259751E2C80EE35876';
+
+export const MongoIDPattern = /^(?=[a-f\d]{24}$)(\d+[a-f]|[a-f]+\d)/i;

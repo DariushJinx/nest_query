9f1a7de8a013a370d0a9361264ccfd20096e4dfc
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-11 16:56:52 +0330

    handle blog category entity

---
+++ src/blogCategory/blogCategory.entity.ts
@@ -0,0 +1,46 @@
+import { BlogEntity } from '../blog/blog.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { AdminEntity } from '../admin/admin.entity';
+
+@Entity({ name: 'blog_category' })
+export class BlogCategoryEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column({ type: 'json' })
+  images: string[];
+
+  @OneToMany(() => BlogEntity, (blog) => blog.category)
+  blogs: BlogEntity[];
+
+  @ManyToOne(() => AdminEntity, (admin) => admin.blog_categories, {
+    eager: true,
+  })
+  @JoinColumn({ name: 'register_id' })
+  register: AdminEntity;
+
+  @Column({ default: 0 })
+  parent: number;
+
+  @Column({ default: 0 })
+  isLast: number;
+
+  @Column({ type: 'json' })
+  tree_cat: string[];
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date;
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  updatedAt: Date;
+}

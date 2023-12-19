354b896c183f61435699bee5474dd46585f189aa
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-19 16:45:29 +0330

    update main.ts with handle static folder

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import * as express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+
+  // Serve static files
+  app.use(express.static(join(__dirname, '..', 'public')));
+
   await app.listen(3000);
 }
 bootstrap();

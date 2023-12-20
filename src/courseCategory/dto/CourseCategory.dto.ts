5895a87516b92b762f990fea7c12400ad3b53a34
Dariush Khazaei <dariushkhazaei629@gmail.com>
2023-12-20 13:00:51 +0330

    handle course category create dto

---
+++ src/courseCategory/dto/CourseCategory.dto.ts
@@ -0,0 +1,22 @@
+import { Expose } from 'class-transformer';
+import { IsDefined, Matches, Length, Allow, IsOptional } from 'class-validator';
+export class CreateCourseCategoryDto {
+  @IsDefined()
+  @Expose()
+  @Length(3, 30, { message: 'عنوان دسته بندی وارد شده صحیح نمی باشد' })
+  title: string;
+  @IsOptional()
+  @Expose()
+  parent: number;
+  @IsOptional()
+  @Expose()
+  tree_cat: string[];
+  @Expose()
+  @IsOptional()
+  @Matches(/(\.png|\.jpg|\.webp|\.jpeg|\.gif)$/, {
+    message: 'فرمت عکس ارسالی صحیح نمی باشد',
+  })
+  filename: string;
+  @Allow()
+  fileUploadPath: string;
+}

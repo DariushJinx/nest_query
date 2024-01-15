81fbb8f3db03c5196fbf7e773246fb9390fffa30
Dariush Khazaei <dariushkhazaei629@gmail.com>
2024-01-15 15:20:04 +0330

    add my sql query

--- nest_query.session.sql
+++ nest_query.session.sql
@@ -1,4 +1,7 @@
 -- Active: 1696281578915@@127.0.0.1@5432@nest_query@public
+
+-- postgresql
+
 select c.*,cc.title as category_title,
 ch.title as chapter_title,
 a.username as register_username
@@ -496,3 +499,287 @@ select c.*,
         left join admin a on blog.author_id = a.id
         left join blog_category bc on blog.category_id = bc.id
         order by blog.id desc
+
+        -- mysql
+
+        
+        SELECT
+        cp.id AS cartPackageId,
+        cp.code AS cartPackageCode,
+        cp.courierPlateNumber AS cartPackageCourierPlateNumber,
+        cp.courierMobNumber AS cartPackageCourierMobNumber,
+        cp.sendTime AS cartPackageSendTime,
+        cp.sendDate AS cartPackageSendDate,
+        cp.shipmentCode AS cartPackageShipmentCode,
+        cp.isPaid AS cartPackageIsPaid,
+        cp.shopId AS shopId,
+        cp.deliveryTime AS deliveryTime,
+        cp.deliveryDate AS deliveryDate,
+        cp.status AS cartPackageStatus,
+        cp.createdAt AS cartPackageCreatedAt,
+        ci.id AS cartItemId,
+        ci.productId AS productId,
+        ci.diversityId AS diversityId,
+        ci.quantity AS quantity,
+        ci.status AS cartItemStatus,
+        ci.listStatus AS cartItemListStatus,
+        ci.createdAt AS cartItemCreatedAt,
+        ci.updatedAt AS cartItemUpdatedAt,
+        p.title AS productTitle,
+        p.shop_type AS productShopType,
+        p.pic AS productPic,
+        p.sendTime AS sendTime,
+        p.deliverySetting AS deliverySetting,
+        p.freeSend AS freeSend,
+        p.box_size AS boxSize,
+        d.diversity AS productDiversity
+        FROM
+        cart_package cp
+        LEFT JOIN
+        cart_item ci ON ci.cartPackage = cp.id
+        LEFT JOIN
+        product p ON  p.id = ci.productId
+        LEFT JOIN 
+        diversity d ON d.id = ci.diversityId
+        WHERE
+            (cp.createdAt BETWEEN ${input.startTime || 0} AND ${
+            input.endTime || 9999999999
+            })
+            AND (cp.status = "${input.status || ""}" OR "${input.status || ""}" = "")
+            AND cp.isPaid = "0"
+            AND cp.shopId = ${input.shopId};
+
+-- SELECT
+--         cp.id AS cartPackageId,
+--         cp.code AS cartPackageCode,
+--         cp.shopId AS shopId,
+--         cp.deliveryTime AS deliveryTime,
+--         cp.deliveryDate AS deliveryDate,
+--         cp.status AS cartPackageStatus,
+--         cp.createdAt AS cartPackageCreatedAt,
+--         ci.id AS cartItemId,
+--         ci.productId AS productId,
+--         ci.diversityId AS diversityId,
+--         ci.quantity AS quantity,
+--         ci.status AS cartItemStatus,
+--         ci.listStatus AS cartItemListStatus,
+--         ci.createdAt AS cartItemCreatedAt,
+--         ci.updatedAt AS cartItemUpdatedAt,
+--         p.title AS productTitle,
+--         p.shop_type AS productShopType,
+--         p.pic AS productPic,
+--         p.sendTime AS sendTime,
+--         p.deliverySetting AS deliverySetting,
+--         p.freeSend AS freeSend,
+--         p.box_size AS boxSize
+--         FROM
+--         cart_package cp
+--         LEFT JOIN
+--         cart_item ci ON ci.cartPackage = cp.id
+--         LEFT JOIN
+--         product p ON  p.id = ci.productId
+--         where cp.shopId = 1 
+--         AND
+--         isPaid = "0"
+
+-- select cp.*,ci.* from cart_package cp
+-- left join cart_item ci on ci.cartPackage = cp.id
+-- where cp.shopId = 1 
+-- AND
+-- isPaid = "0"
+
+
+-- explain analyze
+-- select fo.*,
+--     (
+--         SELECT JSON_ARRAYAGG(
+--                 JSON_OBJECT(
+--                     'id',
+--                     c.id,
+--                     'userId',
+--                     c.userId,
+--                     'addressId',
+--                     c.addressId,
+--                     'completed',
+--                     c.completed,
+--                     'status',
+--                     c.status,
+--                     'sendTime',
+--                     c.sendTime,
+--                     'cart_package',
+--                     (
+--                         SELECT JSON_ARRAYAGG(
+--                                 JSON_OBJECT(
+--                                     'id',
+--                                     cp.id,
+--                                     'userId',
+--                                     cp.userId,
+--                                     'cart',
+--                                     cp.cart,
+--                                     'shopId',
+--                                     cp.shopId,
+--                                     'status',
+--                                     cp.status,
+--                                     'deliveryDate',
+--                                     cp.deliveryDate,
+--                                     'deliveryTime',
+--                                     cp.deliveryTime,
+--                                     'cart_items',
+--                                     (
+--                                         SELECT JSON_ARRAYAGG(
+--                                                 JSON_OBJECT(
+--                                                     'id',
+--                                                     ci.id,
+--                                                     'cartPackage',
+--                                                     ci.cartPackage,
+--                                                     'productId',
+--                                                     ci.productId,
+--                                                     'diversityId',
+--                                                     ci.diversityId,
+--                                                     'quantity',
+--                                                     ci.quantity,
+--                                                     'status',
+--                                                     ci.status,
+--                                                     'listStatus',
+--                                                     ci.listStatus,
+--                                                     'shopId',
+--                                                     ci.shopId,
+--                                                     'createdAt',
+--                                                     ci.createdAt,
+--                                                     'updatedAt',
+--                                                     ci.updatedAt
+--                                                 )
+--                                             )
+--                                         from cart_item ci
+--                                             JOIN cart_package cp ON ci.cartPackage = cp.id
+--                                         WHERE cp.cart = fo.cartId
+--                                     ),
+--                                     'createdAt',
+--                                     cp.createdAt,
+--                                     'updatedAt',
+--                                     cp.updatedAt
+--                                 )
+--                             )
+--                         from cart_package cp
+--                         where cp.cart = (
+--                                 SELECT c.id
+--                                 FROM cart c
+--                                 WHERE c.id = fo.cartId
+--                             )
+--                     ),
+--                     'createdAt',
+--                     c.createdAt,
+--                     'updatedAt',
+--                     c.updatedAt
+--                 )
+--             )
+--         from cart c
+--         where fo.cartId = c.id
+--     ) as carts
+-- from final_order fo;
+-- select fo.* from final_order fo
+-- left join cart c on fo.userId = c.userId
+-- left join cart_package cp on cp.userId = c.userId
+-- left join cart_item ci on cp.id = ci.cartPackage
+-- where
+-- ci.cartPackage = cp.id
+-- and cp.cart = c.id
+-- and fo.cartId = c.id
+-- and fo.isPaid = "1"
+-- select u.*,
+-- coalesce(
+--     (
+--         select array_to_json(array_agg(row_to_json(t)))
+--     from (
+--         select c.title
+--         from course c
+--         where c.id = uc.course_id
+--     ) t
+--     ),
+--     '[]'::json
+-- ) as favorites_courses
+-- from users as u
+-- select fo.* from final_order fo
+-- left join cart c on fo.cartId = c.id
+-- left join cart_package cp on cp.cart = c.id
+-- left join cart_item ci on cp.id = ci.cartPackage
+-- left join return_product rp on rp.cartItemId = ci.id
+-- where
+-- ci.cartPackage = cp.id
+-- and rp.cartItemId = ci.id
+-- and cp.cart = c.id
+-- and fo.cartId = c.id
+-- and fo.isPaid = "1"
+-- select fo.* from final_order fo
+-- left join cart c on fo.cartId = c.id
+-- left join cart_package cp on cp.cart = c.id
+-- left join cart_item ci on cp.id = ci.cartPackage
+-- left join return_product rp on rp.cartItemId = ci.id
+-- where
+-- rp.cartItemId = ci.id
+-- and ci.cartPackage = cp.id
+-- and cp.cart = c.id
+-- and fo.cartId = c.id
+-- and fo.isPaid = "1"
+-- select rp.* from return_product rp
+-- left join cart_item ci on rp.cartItemId = ci.id
+-- left join cart_package cp on ci.cartPackage = cp.id
+-- left join cart c on cp.cart = c.id
+-- left join final_order fo on fo.cartId = c.id
+-- where
+-- rp.cartItemId = ci.id
+-- and ci.cartPackage = cp.id
+-- and cp.cart = c.id
+-- and fo.cartId = c.id
+-- and fo.isPaid = "1"
+-- select fo.*,
+-- coalesce(
+--     (
+--     select array_to_json(array_agg(row_to_json(t)))
+--     from
+--     (
+--         select * from cart c
+--         where c.id = fo.cart_id
+--     ) t
+-- ),
+-- '[]'::json
+-- ) as carts
+-- from final_order fo
+-- select *,
+-- coalesce(
+--     (
+--         select array_to_json(array_agg(row_to_json(t)))
+--         from (
+--             select * from cart c
+--         ) t
+-- ),
+--     '[]'
+-- ) as carts
+-- from final_order
+-- select c.*,
+--     a.username as teacher_name,
+--     cc.title as category_title,
+--     (
+--         select array_to_json(array_agg(row_to_json(t)))
+--         from (
+--             select ch.id,ch.title,ch.text, (
+--             select array_to_json(array_agg(row_to_json(t)))
+--             from (
+--                 select
+--                 e.id,e.title,e.text,e.type,
+--                 e.time,e.video_address
+--                 from episode e
+--                 where e.chapter_id = ch.id
+--             ) t
+--             ) as episodes
+--             from chapter ch
+--             where ch.course_id = c.id
+--         ) t
+--     ) as chapters
+--     from
+--     course as c
+--     left join admin a on c.teacher_id = a.id
+--     left join course_category cc on cc.id = c.category_id
+--     where c.id = 1
+
+    
